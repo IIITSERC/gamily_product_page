57a45cc01c0af98313131e8f06943677edec1dcc
Shivam Khandelwal <skbly7@gmail.com>
2016-04-28 19:06:31 +0530

    Modified email.php to use PHPMailer instead of native mail function

--- email.php
+++ email.php
@@ -1,5 +1,4 @@
 <?php
-
 // Get client IP address (honor X-Forwarded-For)
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -11,16 +10,32 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 
 // Send Email
 if (strpos($_SERVER['HTTP_REFERER'], 'gamily.in') !== false || strpos($_SERVER['HTTP_REFERER'], '127.0.0.1') !== false ) {
-    $success = mail(
-        'shivam.khandelwal@research.iiit.ac.in',
-        'Gamily.in Form Response : ' . $_POST['subject'],
-        $_POST['message']. '\n\n' . $_POST['email']. '\n\n' . $ip
-    );
-    if($success) {
-        echo 'Thanks for writing to us. We will try to get back asap!';
-    }
-    else {
-        echo 'Error. Please send to shivam.khandelwal@research.iiit.ac.in directly. Sorry. :(';
-    }
-}
 
+    require '../PHPMailer/PHPMailerAutoload.php';
+
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->CharSet = 'UTF-8';
+
+    $mail->Host       = 'research.iiit.ac.in';
+    $mail->SMTPDebug  = 0;
+    $mail->SMTPAuth   = true;
+    $mail->Port       = 25;
+    $mail->Username   = 'shivam.khandelwal@research.iiit.ac.in';
+    $mail->Password   = 'WHY YOU THINK I SHOULD SHARE THIS?';
+    $mail->From = $_POST['email'];
+    $mail->FromName = $_POST['name'];
+    $mail->AddAddress('shivam.khandelwal@research.iiit.ac.in', 'Shivam Khandelwal');
+    $mail->AddCC('saikrishna.sripada@research.iiit.ac.in', 'Saikrishna Sripada');
+    $mail->IsHTML(true);
+    $mail->Subject = 'Gamily.in contact form : ' . $_POST['subject'];
+    $mail->Body = nl2br($_POST['message']) . '<br><br>Sent from IP : ' . $ip;
+    if(!$mail->Send())
+    {
+       echo 'Error. Please send to shivam.khandelwal@research.iiit.ac.in directly. Sorry. :(';
+       exit(0);
+    }
+
+    echo 'Thanks for writing to us. We will try to get back asap!';
+
+}
